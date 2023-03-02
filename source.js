53cc08a8abcb381574207eadddf909bb7e698919
mdowns <michaeljdowns99@gmail.com>
2023-03-02 09:02:26 -0700

    Set up File Structure for Project

---
+++ source.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const mongodb = require("./db/connect");
+const bodyParser = require("body-parser");
+
+
+const app = express();
+
+const port = process.env.PORT || 8080;
+
+app 
+.use(bodyParser.json())
+.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow Origin", "*");
+    next();
+})
+
+.use("/", require("./routes"));
+
+app.listen(port);
+console.log(`Connected on ${port}`);
+
+// mongodb.initDb((err, mongodb, next) => {
+//     if (err) {
+//       console.log(err);
+//     } else {
+//       app.listen(port);
+//       console.log(`Connected to DB and listening on ${port}`);
+//     }
+//   });
