1eb66af886a47ff481b3278290a27540c9f960da
carly-west <carly.west@byui.edu>
2023-03-10 10:22:00 -0700

    added GET user route

--- controller/user.js
+++ controller/user.js
@@ -3,7 +3,9 @@ const ObjectId = require("mongodb").ObjectId;
 const valid = require("../helper/index");
 
 const getUserLogin = async (req, res) => {
-  res.status(200).json("Get User Login");
+  // Will work once Oauth is implemented
+  res.send(JSON.stringify(req.oidc.user));
+
 };
 
 const getUserLogout = async (req, res) => {
@@ -15,15 +17,15 @@ const createUser = async (req, res) => {
 };
 
 const updateUser = async (req, res) => {
-  try{
+  try {
     const userId = new ObjectId(req.params.requestId);
-    const result = new mongodb.getDb().db('rexcube').collection('users').replaceOne({_id:userId}, req.body);
+    const result = new mongodb.getDb().db('rexcube').collection('users').replaceOne({ _id: userId }, req.body);
     if (result.modifiedCount != 0) {
-        res.status(204).send();
-    } 
-} catch {
+      res.status(204).send();
+    }
+  } catch {
     res.status(500).send(err.message);
-}
+  }
 };
 
 const deleteUser = async (req, res) => {
@@ -36,13 +38,13 @@ const deleteUser = async (req, res) => {
       .getDb()
       .db('rexcube')
       .collection('users')
-      .deleteOne({ _id: userIdString});
+      .deleteOne({ _id: userIdString });
 
     console.log(`Results Deleted: ${result.deletedCount} `);
-      if(result.deletedCount > 0){
-        res.status(204).send();
-        console.log(`Info was Deleted. Items Deleted ${result.deletedCount}`);
-      }
+    if (result.deletedCount > 0) {
+      res.status(204).send();
+      console.log(`Info was Deleted. Items Deleted ${result.deletedCount}`);
+    }
   } catch (err) {
     res.status(200).json(err.message);
   }
