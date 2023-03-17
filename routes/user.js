c6bb33573f4764830359d2cc8ac0b44b22e89f8d
carly-west <carly.west@byui.edu>
2023-03-17 13:11:37 -0600

    update users

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,11 @@
 const routes = require('express').Router();
 const controller = require('../controller/user');
+const { requiresAuth } = require('express-openid-connect');
+
 
 routes.get('/', controller.getUserLogin);
 routes.get('/', controller.getUserLogout);
-routes.post('/', controller.createUser);
+routes.post('/', requiresAuth(), controller.createUser);
 routes.delete('/id', controller.deleteUser);
 routes.put('/:userId', controller.updateUser);
 
